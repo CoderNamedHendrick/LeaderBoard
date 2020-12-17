944dd871cd607bdf4f7a20b8c1ba326b7b399cde
Rookiecoder17 <52936414+Rookiecoder17@users.noreply.github.com>
2020-12-17 14:14:43 +0000

    Fixed some typo

--- app/src/main/java/com/example/leaderboard/SubmitProjectService.java
+++ app/src/main/java/com/example/leaderboard/SubmitProjectService.java
@@ -10,7 +10,7 @@ public interface SubmitProjectService {
     @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     @FormUrlEncoded
     Call<Void> submitProject(@Field("entry.1824927963") String emailAddress,
-                             @Field(" entry.1877115667") String firstName,
-                             @Field(" entry.2006916086") String lastName,
+                             @Field("entry.1877115667") String firstName,
+                             @Field("entry.2006916086") String lastName,
                              @Field("entry.284483984") String linkToProject);
 }
